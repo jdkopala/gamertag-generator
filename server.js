4b4beaf5bbff5aa48241cd0f57d5b700b9ffcab0
Jesse Kopala <jdkopala@gmail.com>
2022-08-24 13:25:31 -0600

    Server setup, render page to browser

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const { restart } = require('nodemon');
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+
+// Middleware
+
+app.use(morgan("common"));
+
+//
+
+app.get("/", (req, res) => {
+  res.render("home.ejs")
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`)
+})
